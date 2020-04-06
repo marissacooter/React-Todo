f935223ea54152afd383eff8e2fcae51c9fd1eaf
Marissa <marissacooterl@gmail.com>
2020-04-06 17:06:46 -0500

    testing pushes again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: "Organize Garage",
+          id: uuidv4(),
+          completed: false,
+        },
+        {
+          task: "Bake Cookies",
+          id: uuidv4(),
+          completed: false,
+        },
+      ],
+    };
+  }
+  addToDo = task => {
+    const newItem = {
+      task: task,
+      id: uuidv4(),
+      completed: false,
+    }
+    this.setState({
+      //.concat adds and returns new array plus old items
+      todos: this.state.todos.concat(newItem)
+    })
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos} />
+        <TodoForm addToDo={this.addToDo} />
       </div>
     );
   }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,1 +1,38 @@
-import React from 'react';
+import React from "react";
+
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      newToDo: "",
+    };
+  }
+  handleChanges = (e) => {
+    e.preventDefault();
+    this.setState({ newToDo: e.target.value });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          name="todo"
+          value={this.state.newToDo}
+          onChange={this.handleChanges}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            this.props.addToDo(this.state.newToDo);
+          }}
+        >
+          Add
+        </button>
+        <button onClick={this.handleClear}>Clear</button>
+      </form>
+    );
+  }
+}
+
+export default TodoForm;
